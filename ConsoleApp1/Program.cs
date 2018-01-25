d3d1feab3a458f12735ca17c0ebcf87fc6b59e4a
Joshua Freeman <josfree@ut.utm.edu>
2018-01-25 11:00:58 -0600

    Guess a number

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            int randNum = random.Next(0, 100);
+            int userGuess = 0;
+            int totalGuesses = 1;
+
+            Console.WriteLine("I am thinking of a number between 0 and 100.  Guess what it is.");
+
+            while (userGuess != randNum)
+            {
+                userGuess = Convert.ToInt32(Console.ReadLine());
+                if (userGuess < randNum)
+                {
+                    Console.WriteLine("That's too low. Guess again.");
+                    totalGuesses++;
+                }
+                else if (userGuess > randNum)
+                {
+                    Console.WriteLine("That's too high. Guess again.");
+                    totalGuesses++;
+                }
+            }
+            Console.WriteLine("That's it! The answer was " + randNum + ". It took you " + totalGuesses + " guesses to guess the correct number.");
+
             Console.ReadLine();
         }
     }
